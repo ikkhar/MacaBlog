65855b2e6d2efe4c5aa25b4e26e87ee49577761d
Pierre Guillec <pierre@pierreguillec.com>
2020-05-07 18:13:13 +0200

    transfert back

---
+++ BlogServer/src/test/java/fr/macademia/macablog/MacablogApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.macademia.macablog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacablogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
