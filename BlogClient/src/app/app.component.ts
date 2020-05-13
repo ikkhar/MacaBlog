d9c3067e7c7e4c3a8f62c5975833dfd7d06f52fb
Pierre Guillec <pierre@pierreguillec.com>
2020-05-13 18:48:13 +0200

    commit avant cli update

--- BlogClient/src/app/app.component.ts
+++ BlogClient/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'BlogClient';
+
+  isShow: boolean;
+  topPosToStartShowing = 100;
+  @HostListener('window:scroll')
+  checkScroll() {
+
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    console.log('[scroll]', scrollPosition);
+    this.isShow = scrollPosition >= this.topPosToStartShowing;
+  }
+  gotoTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
 }
--- BlogClient/src/app/app.module.ts
+++ BlogClient/src/app/app.module.ts
@@ -1,14 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {RouterModule, Routes} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BlogListComponent } from './blog-list/blog-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BlogComponent} from './blog/blog.component';
+import {MatProgressBarModule} from '@angular/material';
+
+
+
+
+
+
+const blogRoutes: Routes = [
+{ path: 'home', component: HomeComponent },
+{ path: 'blog', component: BlogComponent },
+{ path: '', redirectTo: 'home', pathMatch: 'full' },
+{ path: '**', redirectTo: 'home' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogListComponent,
+    HomeComponent,
+    BlogComponent,
+
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    HttpClientModule,
+    RouterModule.forRoot(blogRoutes),
+    MatProgressBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
