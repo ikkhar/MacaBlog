25c6e7dcc965dbc8cd293cd67abb1efc7600a871
csigault <sigaultc@gmail.com>
2020-05-13 17:43:13 +0200

    Component Topics

--- BlogClient/src/app/app.module.ts
+++ BlogClient/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatListModule} from '@angular/material';
+import { MatListModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
