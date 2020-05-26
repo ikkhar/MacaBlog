b49a1ee2aa34320de1f2b30072a5d47554d514ae
ben2206 <58642932+ben2206@users.noreply.github.com>
2020-05-26 13:20:02 +0200

    update

--- BlogClient/src/app/app.module.ts
+++ BlogClient/src/app/app.module.ts
@@ -2,8 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import {MatExpansionModule} from '@angular/material/expansion';
-
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -11,18 +10,18 @@ import { HeaderComponent } from './home/header/header.component';
 import { FooterComponent } from './home/footer/footer.component';
 import { BlogListComponent } from './blog-list/blog-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {BlogComponent} from './blog/blog.component';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {FlexLayoutModule} from '@angular/flex-layout';
+import { BlogComponent } from './blog/blog.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { BlogEntryComponent } from './blog/blog-entry/blog-entry.component';
 import { ShareComponent } from './blog/share/share.component';
 import { LastEntriesComponent } from './blog/last-entries/last-entries.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatListModule} from '@angular/material/list';
+import { MatListModule } from '@angular/material/list';
 import { TopicsComponent } from './home/topics/topics.component';
 import { TopicsPhoneComponent } from './home/topics/topics-phone/topics-phone.component';
 
