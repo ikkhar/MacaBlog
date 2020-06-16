de1bbd436d8a22d808cf15926be5e8fbbe5abef5
csigault <60656097+csigault@users.noreply.github.com>
2020-06-16 16:47:59 +0200

    feature/CS-innerHTML (#47)
    Modification du json articles, modification du component details-article.component (html & ts)

--- BlogClient/src/app/details-article/details-article.component.ts
+++ BlogClient/src/app/details-article/details-article.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Article } from '../models/article';
 import { ArticleService } from '../services/articles.service';
 import { ActivatedRoute, Data, Router } from '@angular/router';
+import { HighlightService } from '../services/highlight.service'
 
 
 @Component({
@@ -15,8 +16,19 @@ export class DetailsArticleComponent implements OnInit {
   id: number;
   article: Article;
 
+  highlighted: boolean = false;
 
-  constructor(private articleService: ArticleService, private route: ActivatedRoute, private router: Router) {}
+  constructor(private articleService: ArticleService, private route: ActivatedRoute, private router: Router, private highlightService: HighlightService) {}
+
+  /**
+   * Highlight blog post when it's ready
+   */
+  ngAfterViewChecked() {
+    
+    this.highlightService.highlightAll();
+    this.highlighted = true;
+  
+}
 
   async ngOnInit() {
 
