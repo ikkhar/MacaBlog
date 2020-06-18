35c7856d521803dfb33f77e1d4bca64b66e9b9b5
csigault <sigaultc@gmail.com>
2020-06-18 16:31:17 +0200

    feature/CS-links-last-article
    Ajouts de lien, et simulation auteur et topic

--- BlogClient/src/app/details-article/details-article.component.ts
+++ BlogClient/src/app/details-article/details-article.component.ts
@@ -18,8 +18,13 @@ export class DetailsArticleComponent implements OnInit {
   public isloading: boolean;
   id: number;
   article: Article;
+  
 
   highlighted: boolean = false;
+
+  // Test Keywords
+  keywords:string = 'jaimepasjava';
+  auteur: string = 'Mazen Gharbi';
 
   constructor(private articleService: ArticleService, private route: ActivatedRoute, private router: Router, private highlightService: HighlightService) {
     this.progresValue = 0;
