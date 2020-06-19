015bb24d2f51316c6752cb8bf4449c605fbfc014
SihemSissi <bounouasihem@gmail.com>
2020-06-19 13:46:23 +0200

    suppression methodebug top10by... dans articles respository

--- BlogServer/src/main/java/fr/macademia/macablog/model/repositories/ArticlesRepository.java
+++ BlogServer/src/main/java/fr/macademia/macablog/model/repositories/ArticlesRepository.java
@@ -1,6 +1,5 @@
 package fr.macademia.macablog.model.repositories;
 
-
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -14,51 +13,55 @@ import org.springframework.data.repository.query.Param;
 import fr.macademia.macablog.model.entities.Articles;
 import fr.macademia.macablog.model.entities.Auteur;
 
-
-
 @Repository
-public interface ArticlesRepository extends JpaRepository <Articles, Long>{
+public interface ArticlesRepository extends JpaRepository<Articles, Long> {
 	Optional<Articles> findById(Long id);
-	void deleteById(Long id);
-	List<Articles> findAll();
-	List<Articles> findAllByOrderByIdAsc();
-	List<Articles> findAllByOrderByIdDesc();//trouvé tout les articles dans l'ordre decroissant
-List <Articles>findFirst10ByThematiquesIdOrderByIdDesc(Long id);
-List <Articles>findFirst10OrderByIdDesc(Long id);
-//List<Article> findTop3ByOrderBySalaryDesc();
-	//	List<Articles> par Auteur Id;
-	
-	Optional<List<Articles>>  findByAuteurListId(Long id);
-	
-	
-	//Methodes pour trouver la liste des articles par ID  thematiques ou sous thematiques
-		List<Articles> findByThematiquesId(Long id);
 
-		List<Articles> findBySubThematiquesId(Long id);
-		List<Articles>findByThematiquesIdOrderByIdDesc(Long id);
-	
-	
-	//Methodes pour trouver la liste des articles par thematiques ou sous thematiques
+	void deleteById(Long id);
+
+	List<Articles> findAll();
+
+	List<Articles> findAllByOrderByIdAsc();
+
+	List<Articles> findAllByOrderByIdDesc();// trouvé tout les articles dans l'ordre decroissant
+
+	List<Articles> findFirst10ByThematiquesIdOrderByIdDesc(Long id);
+
+	// List<Articles> par Auteur Id;
+
+	Optional<List<Articles>> findByAuteurListId(Long id);
+
+	// Methodes pour trouver la liste des articles par ID thematiques ou sous thematiques
+	List<Articles> findByThematiquesId(Long id);
+
+	List<Articles> findBySubThematiquesId(Long id);
+
+	List<Articles> findByThematiquesIdOrderByIdDesc(Long id);
+
+	// Methodes pour trouver la liste des articles par thematiques ou sous
+	// thematiques
 	Optional<List<Articles>> findByThematiquesName(String thematiques);
 
 	Optional<List<Articles>> findBySubThematiquesSubName(String subThematiques);
 
 //	ArrayList<Articles> findByKeywords(Long id);
 //	Méthode pour l'auto-complétion
-	@Query(value="select name from articles where name like %:keyword%", nativeQuery = true)
-	Optional<List<String>> search(@Param("keyword")String keyword);
-	
-	//Methode pour trouver un article par son titre
+	@Query(value = "select name from articles where name like %:keyword%", nativeQuery = true)
+	Optional<List<String>> search(@Param("keyword") String keyword);
+
+	// Methode pour trouver un article par son titre
 	Optional<Articles> findByTitle(String title);
+
 //	Page<Articles> findBySubThematiquesId(Pageable requestPage);
 	Page<Articles> findAllByOrderByIdDesc(Pageable requestPage);
-	Page<Articles>findByThematiquesId(Pageable requestPage,Long id);// affiche dans l'ordre croissant
-	
-	
-	Page<Articles>findBySubThematiquesId(Pageable requestPage,Long id);// affiche dans l'ordre croissant
-	
-	Page<Articles>findByThematiquesIdOrderByIdDesc(Pageable requestPage,Long id);// affiche dans l'ordre decroissant 
 
-	Page<Articles>findBySubThematiquesIdOrderByIdDesc(Pageable requestPage,Long id);// affiche dans l'ordre decroissant 
+	Page<Articles> findByThematiquesId(Pageable requestPage, Long id);// affiche dans l'ordre croissant
+
+	Page<Articles> findBySubThematiquesId(Pageable requestPage, Long id);// affiche dans l'ordre croissant
+
+	Page<Articles> findByThematiquesIdOrderByIdDesc(Pageable requestPage, Long id);// affiche dans l'ordre decroissant
+
+	Page<Articles> findBySubThematiquesIdOrderByIdDesc(Pageable requestPage, Long id);// affiche dans l'ordre
+																						// decroissant
 
 }
